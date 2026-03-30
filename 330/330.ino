28e377122e62649630cf6982eaf3d402b165c09e
Ganzhe2028 <gztxwd200828@qq.com>
2026-03-30 16:19:24 +0800

    feat: add motor control logic and sensor testing sketches for robotics project

---
+++ 330/330.ino
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+
+// PWM 调速：HIGH 只是数值 1，占空比太低，电机往往看起来“完全不动”。全速用 255。
+const uint8_t kMotorPwm = 255;
+
+int speed1 = 5;
+int direction1 = 4;
+int speed2 = 6;
+int direction2 = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(direction1, OUTPUT);
+  pinMode(direction2, OUTPUT);
+  pinMode(speed1, OUTPUT);
+  pinMode(speed2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  forward();
+  delay(1000);
+  backward();
+  delay(1000);
+  left();
+  delay(300);
+  right();
+  delay(600);
+  stop();
+  delay(5000);
+}
+
+void forward() {
+  digitalWrite(direction1, LOW);
+  digitalWrite(direction2, HIGH);
+  analogWrite(speed1, kMotorPwm); // left
+  analogWrite(speed2, 230); // right
+}
+
+void backward() {
+  digitalWrite(direction1, HIGH);
+  digitalWrite(direction2, LOW);
+  analogWrite(speed1, kMotorPwm);
+  analogWrite(speed2, 230);
+
+  // LOW = motor backward && full speed turning counterclockwise
+}
+
+void left() {
+
+  digitalWrite(direction1, HIGH);
+  digitalWrite(direction2, HIGH);
+  analogWrite(speed1, kMotorPwm);
+  analogWrite(speed2, 230);
+}
+
+void right() {
+
+  digitalWrite(direction1, LOW);
+  digitalWrite(direction2, LOW);
+  analogWrite(speed1, kMotorPwm);
+  analogWrite(speed2, 230);
+}
+
+void stop() {
+  digitalWrite(direction1, LOW);
+  digitalWrite(direction2, LOW);
+  analogWrite(speed1, 0);
+  analogWrite(speed2, 0);
+}
