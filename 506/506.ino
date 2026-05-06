14206c3c7fc7b56e00f3a0b9ed80d1e3a99cfc70
Ganzhe2028 <gztxwd200828@qq.com>
2026-05-06 09:35:15 +0800

    最新的是506.目前还在测试但是应该没什么错的。最新版

---
+++ 506/506.ino
@@ -0,0 +1,124 @@
+#include <Arduino.h>
+#include <SharpDistSensor.h>
+
+// PWM 调速：HIGH 只是数值 1，占空比太低，电机往往看起来“完全不动”。全速用 255。
+const uint8_t high = 240;
+const uint8_t high1 = 255;
+const uint8_t low = 0;
+const byte medianFilterWindowSize = 5;
+const byte LEFT_DIST_SENSOR_PIN = A0;
+const byte RIGHT_DIST_SENSOR_PIN = A2;
+
+SharpDistSensor leftDistSensor(LEFT_DIST_SENSOR_PIN, medianFilterWindowSize);
+SharpDistSensor rightDistSensor(RIGHT_DIST_SENSOR_PIN, medianFilterWindowSize);
+
+int LeftSensor=3;
+int RightSensor=2;
+int speed1 = 5;
+int direction1 = 7;
+int speed2 = 6;
+int direction2 = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LeftSensor, INPUT_PULLUP);
+  pinMode(RightSensor, INPUT_PULLUP);
+  pinMode(direction1, OUTPUT);
+  pinMode(direction2, OUTPUT);
+  pinMode(speed1, OUTPUT);
+  pinMode(speed2, OUTPUT);
+}
+
+void loop() {
+  int leftLine = digitalRead(LeftSensor);
+  int rightLine = digitalRead(RightSensor);
+  int leftRaw = analogRead(LEFT_DIST_SENSOR_PIN);
+  int rightRaw = analogRead(RIGHT_DIST_SENSOR_PIN);
+  unsigned int leftDistance = leftDistSensor.getDist();
+  unsigned int rightDistance = rightDistSensor.getDist();
+
+  Serial.print("L:");
+  Serial.print(leftLine);
+  Serial.print(" R:");
+  Serial.print(rightLine);
+  Serial.print(" A0:");
+  Serial.print(leftRaw);
+  Serial.print(",");
+  Serial.print(leftDistance);
+  Serial.print("mm A2:");
+  Serial.print(rightRaw);
+  Serial.print(",");
+  Serial.print(rightDistance);
+  Serial.println("mm");
+
+  if((leftLine==0)&&(rightLine==0)){
+    if (leftDistance < 600) {
+      right();
+      delay(50);
+      forward();
+    } else if (rightDistance < 600) {
+      left();
+      delay(50);
+      forward();
+    } else {
+      forward();
+    }
+  }else if((leftLine==1)&&(rightLine==0)){
+    backward();
+    delay(700);
+    right();
+    delay(500);
+  }else if((leftLine==0)&&(rightLine==1)){
+    right();
+    backward();
+    delay(700);
+    left();
+    delay(500);
+  }else if((leftLine==1)&&(rightLine==1)){
+    backward();
+    delay(700); 
+    left();
+    delay(500);
+  }
+}
+
+void forward() {
+  digitalWrite(direction1, HIGH);
+  digitalWrite(direction2, LOW);
+  analogWrite(speed1, high);
+  analogWrite(speed2, high);
+}
+
+void backward() {
+  digitalWrite(direction1, LOW);
+  digitalWrite(direction2, HIGH);
+  analogWrite(speed1, high1);
+  analogWrite(speed2, high1);
+
+  // LOW = motor backward && full speed turning counterclockwise
+}
+
+void left() {
+
+  digitalWrite(direction1, HIGH);
+  digitalWrite(direction2, HIGH);
+  analogWrite(speed1, high1);
+  analogWrite(speed2, high1);
+}
+
+void right() {
+
+  digitalWrite(direction1, LOW);
+  digitalWrite(direction2, LOW);
+  analogWrite(speed1, high1);
+  analogWrite(speed2, high1);
+}
+
+void stop() {
+
+  digitalWrite(direction1, HIGH);
+  digitalWrite(direction2, HIGH);
+  analogWrite(speed1, low);
+  analogWrite(speed2, low);
+}
--- SensorTestMini/SensorTestMini.ino
+++ SensorTestMini/SensorTestMini.ino
@@ -1,8 +1,20 @@
 // =============================================
 // Minimal Sensor Test - 最小化传感器测试
 // 左: Pin 2 | 右: Pin 3
+// 距离: A0 | A2
 // Serial Monitor (9600)
 // =============================================
+
+#include <SharpDistSensor.h>
+
+// Window size of the median filter (odd number, 1 = no filtering)
+const byte medianFilterWindowSize = 5;
+
+const byte DIST_SENSOR_A0_PIN = A0; //Left sensor is on A0 to avoid conflict with Left sensor on Pin 2 
+const byte DIST_SENSOR_A2_PIN = A2; //Right sensor is on A2 to avoid conflict with Left sensor on Pin 2
+
+SharpDistSensor distSensorA0(DIST_SENSOR_A0_PIN, medianFilterWindowSize);
+SharpDistSensor distSensorA2(DIST_SENSOR_A2_PIN, medianFilterWindowSize);
 
 const int LEFT_SENSOR  = 2;
 const int RIGHT_SENSOR = 3;
@@ -11,17 +23,30 @@ void setup() {
   Serial.begin(9600);
   pinMode(LEFT_SENSOR,  INPUT_PULLUP);
   pinMode(RIGHT_SENSOR, INPUT_PULLUP);
-  Serial.println("L:左 R:右 (0=遮住 1=光亮)");
+  Serial.println("L/R:数字sensor (0=遮住 1=光亮) | A0/A2:raw,mm");
 }
 
 void loop() {
   int L = digitalRead(LEFT_SENSOR);
   int R = digitalRead(RIGHT_SENSOR);
+  int rawA0 = analogRead(DIST_SENSOR_A0_PIN); //left
+  int rawA2 = analogRead(DIST_SENSOR_A2_PIN); //right
+  unsigned int distA0 = distSensorA0.getDist();
+  unsigned int distA2 = distSensorA2.getDist();
   
   Serial.print("L:");
   Serial.print(L);
   Serial.print(" R:");
-  Serial.println(R);
+  Serial.print(R);
+  Serial.print(" A0:");
+  Serial.print(rawA0);
+  Serial.print(",");
+  Serial.print(distA0);
+  Serial.print("mm A2:");
+  Serial.print(rawA2);
+  Serial.print(",");
+  Serial.print(distA2);
+  Serial.println("mm");
   
   delay(200);
 }
